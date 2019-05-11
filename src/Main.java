3a2c6d5928ac377ebd7aeafd9ef690b37afeb5eb
jackhu <n10176250@qut.edu.au>
2019-05-11 22:52:23 +1000

    test git

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        //test git
+        //main program where starting program goes
     }
 }
