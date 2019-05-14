9e6950c7b2e55cebbe0a5f7e092a56c6f60fe7ab
Soo Bin Lee <mariddo3sb@gmail.com>
2019-05-14 18:49:05 +1000

    Shape Ellipse edited & Rectangle added

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,20 @@
 import shapes.Ellipse;
+import shapes.Rectangle;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
 
 public class Main extends JPanel {
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
         JFrame f = new JFrame("Draw a Red Line");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(300, 300);
         f.setLocation(300, 300);
         f.setResizable(true);
 
-        Ellipse e = new Ellipse();
-        f.add(e);
+        f.add(new Ellipse(Color.BLACK, Color.BLUE));
+        //f.add(new Rectangle(Color.BLACK, null));
+
         f.setVisible(true);
     }
 
--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -7,7 +7,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import java.util.ArrayList;
 
-public class Ellipse extends JPanel {
+public class Rectangle extends JPanel {
 
     private ArrayList<Point> pointStart = new ArrayList<Point>();
     private ArrayList<Point> pointEnd = new ArrayList<Point>();
@@ -17,7 +17,7 @@ public class Ellipse extends JPanel {
     private Color penColor;
     private Color fillColor;
 
-    public Ellipse(Color penColor, Color fillColor) {
+    public Rectangle(Color penColor, Color fillColor) {
         this.penColor = penColor;
         this.fillColor = fillColor;
         mouseListen();
@@ -29,10 +29,10 @@ public class Ellipse extends JPanel {
             int index = pointStart.indexOf(point);
             if (fillColor != null) {
                 g.setColor(fillColor);
-                g.fillOval(point.x, point.y, width.get(index), height.get(index));
+                g.fillRect(point.x, point.y, width.get(index), height.get(index));
             }
             g.setColor(penColor);
-            g.drawOval(point.x, point.y, width.get(index), height.get(index));
+            g.drawRect(point.x, point.y, width.get(index), height.get(index));
         }
     }
 
@@ -60,9 +60,23 @@ public class Ellipse extends JPanel {
                 Point start = pointStart.get(pointEnd.indexOf(e.getPoint()));
                 width.add(pointIndex, e.getPoint().x - start.x);
                 height.add(pointIndex,e.getPoint().y - start.y);
+
+//                int newX = start.x;
+//                int newY = start.y;
+//                int newWidth = width.get(pointIndex);
+//                int newHeight = height.get(pointIndex);
+//                if (width.get(pointIndex) < 0) {
+//                    newX = start.x + width.get(pointIndex);
+//                    width.add(pointIndex, -newWidth);
+//                }
+//                if (height.get(pointIndex) < 0) {
+//                    newY = start.y + height.get(pointIndex);
+//                    height.add(pointIndex, -newHeight);
+//                }
+//                start.setLocation(newX, newY);
+//                pointStart.add(pointIndex, start);
                 repaint();
             }
         });
     }
-
 }
